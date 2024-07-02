561fa8e39d136aec627cd7d62858d5dc03d20097
Aaron Jackson <aaron@aaronsplace.co.uk>
2024-07-03 00:36:11 +0100

    Update train departure board for 10 seconds with clock...

--- commands/trains.js
+++ commands/trains.js
@@ -16,21 +16,37 @@ module.exports = function () {
     this.onDiscordMessage = (message) => {
 	if (!message.content.startsWith("!trains")) return;
 
-	let board = "**Departures from Nottingham Railway Station**\n";
-	board += "```\n";
-	this.departures.forEach(row => {
-	    board += row.std.padEnd(6);
-	    board += row.destination.padEnd(32).substring(0, 31);
-	    board += row.platform.padEnd(4);
-	    board += row.etd.padEnd(6);
-	    board += "\n";
-	});
-	if (this.departures.length == 0) {
-	    board += "No trains pending departure :'(";
-	}
-	board += "```";
+	let getBoard = () => {
+	    let board = "**Departures from Nottingham Railway Station**\n";
+	    board += "```\n";
+	    this.departures.forEach(row => {
+		board += row.std.padEnd(6);
+		board += row.destination.padEnd(32).substring(0, 31);
+		board += row.platform.padEnd(4);
+		board += row.etd.padEnd(6);
+		board += "\n";
+	    });
+	    if (this.departures.length == 0) {
+		board += "No trains pending departure :'(";
+	    } else {
+		const time = new Date().toString()
+		      .split(' ')[4].padStart(28).padEnd(48);
+		board += "\n" + time;
+	    }
+	    board += "```";
 
-	message.reply(board)
+	    return board;
+	};
+
+	message.reply(getBoard())
+	    .then((response) => {
+		// Refresh for...
+		for (let timer = 1; timer <= 10; timer++) {
+		    setTimeout((m) => {
+			m.edit(getBoard());
+		    }, timer * 1000, response);
+		}
+	    })
 	    .catch(err => {
 		this.mqttClient.publish('nh/discord/error', err);
 	    });
