7a2dcc7edd1b4c0fae1110441e4b46875c50d978
JSON Alexander <61407072+jasonalexander-ja@users.noreply.github.com>
2025-06-27 01:16:57 +0100

    Added alerting for Furry Radiological Response Unit when high CPM is detected.

--- commands/radiation.js
+++ commands/radiation.js
@@ -3,28 +3,52 @@ const conf = require('../config.json');
 
 module.exports = function () {
     this.cpm = {};
+    this.discordClient = null;
 
     this.onMqttMessage = (topic, message) => {
-	if (!topic.startsWith('nh/radiation/')) return;
+    if (!topic.startsWith('nh/radiation/')) return;
 
-	const id = topic.replace('nh/radiation/', '');
-	this.cpm[id] = message.toString();
+    const id = topic.replace('nh/radiation/', '');
+    this.cpm[id] = message.toString();
+
+    // If above 100cpm, alert Furry Radiological Response Unit
+    if (Number(this.cpm[id]) < 100) return;
+
+    const guild = this.discordClient
+      .guilds.cache.get(conf.primaryGuild);
+
+    // Get user Ids of FRRU members 
+      guild.members.fetch()
+      .then(members => {
+        const mentions = members.filter(member => conf.FRRUUsersnames.includes(member.user.username))
+          .map(v => `<@${member.user.id}>`).join(" ");
+        if (!mentions) return;
+        this.discordClient
+          .channels.fetch(conf.notificationChannel)
+          .then(channel => {
+            channel.send({
+              content: `☢️ Unusually high CPM detected in space ☢️ \r\nLast reading: ${this.cpm[id]}\r\n` +
+              `🐾☢️ Alerting Furry Radiological Response Unit 🐾☢️: ${mentions}`,
+              flags: [ 4096 ] // silenced
+            });
+          });
+      });
     };
 
     this.onDiscordMessage = (message) => {
-	if (!message.content.startsWith("!radiation")) return;
+  if (!message.content.startsWith("!radiation")) return;
 
-	var radiationEmbed = new EmbedBuilder()
-	    .setTitle("Radiation")
-	    .setDescription("Here are the last radiation CPM readings in the space.");
+  var radiationEmbed = new EmbedBuilder()
+      .setTitle("Radiation")
+      .setDescription("Here are the last radiation CPM readings in the space.");
 
-	for (const [k, v] of Object.entries(this.cpm)) {
-	    radiationEmbed.addFields(
-		{ name: k, value: v, inline: true}
-	    );
-	};
+  for (const [k, v] of Object.entries(this.cpm)) {
+      radiationEmbed.addFields(
+    { name: k, value: v, inline: true}
+      );
+  };
 
-	message.reply({ embeds: [ radiationEmbed ]});
-	message.react('☢️');
+  message.reply({ embeds: [ radiationEmbed ]});
+  message.react('☢️');
     };
 };
