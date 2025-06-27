5cc4688486db7ae0e1e6efbcd1592f11b4d674be
JSON Alexander <61407072+jasonalexander-ja@users.noreply.github.com>
2025-06-27 10:37:55 +0100

    Fixing indentation.

--- commands/radiation.js
+++ commands/radiation.js
@@ -2,53 +2,64 @@ const { EmbedBuilder } = require('discord.js');
 const conf = require('../config.json');
 
 module.exports = function () {
-	this.cpm = {};
-	this.discordClient = null;
+    this.cpm = {};
+    this.discordClient = null;
 
-	this.onMqttMessage = (topic, message) => {
-		if (!topic.startsWith('nh/radiation/')) return;
+    this.onMqttMessage = (topic, message) => {
+	    if (!topic.startsWith('nh/radiation/')) return;
 
-		const id = topic.replace('nh/radiation/', '');
-		this.cpm[id] = message.toString();
+	    const id = topic.replace('nh/radiation/', '');
+	    this.cpm[id] = {
+		    cpm: message.toString(),
+		    timestamp: new Date()
+	    };
 
-		// If above 100cpm, alert Furry Radiological Response Unit
-		if (Number(this.cpm[id]) < 100) return;
+	    // If above 100cpm, alert Furry Radiological Response Unit
+	    if (Number(this.cpm[id.cpm]) < 100) return;
 
-		const guild = this.discordClient
-			.guilds.cache.get(conf.primaryGuild);
+	    // If there are other reports from the past 2 hours don't alert
+	    let twoHourAgo = new Date();
+	    twoHourAgo.setHours(now.getHours() - 2);
+	    let isOtherEntries = Object.entries(this.cpm)
+		    .some(([_k, v]) => v.timestamp > twoHourAgo && v.cpm > 100);
+	    if(isOtherEntries) return; 
 
-		// Get user Ids of FRRU members 
-		guild.members.fetch()
-			.then(members => {
-				const mentions = members.filter(member => conf.FRRUUsersnames.includes(member.user.username))
-					.map(v => `<@${member.user.id}>`).join(" ");
-				if (!mentions) return;
-				this.discordClient
-					.channels.fetch(conf.notificationChannel)
-					.then(channel => {
-						channel.send({
-							content: `☢️ Unusually high CPM detected in space ☢️ \r\nLast reading: ${this.cpm[id]}\r\n` +
-								`🐾☢️ Alerting Furry Radiological Response Unit 🐾☢️: ${mentions}`,
-							flags: [4096] // silenced
-						});
-					});
-			});
-	};
 
-	this.onDiscordMessage = (message) => {
-		if (!message.content.startsWith("!radiation")) return;
+	    const guild = this.discordClient
+		    .guilds.cache.get(conf.primaryGuild);
 
-		var radiationEmbed = new EmbedBuilder()
-			.setTitle("Radiation")
-			.setDescription("Here are the last radiation CPM readings in the space.");
+	    // Get user Ids of FRRU members 
+	    guild.members.fetch()
+		    .then(members => {
+			    const mentions = members.filter(member => conf.FRRUUsersnames.includes(member.user.username))
+				    .map(v => `<@${member.user.id}>`).join(" ");
+			    if (!mentions) return;
+			    this.discordClient
+				    .channels.fetch(conf.notificationChannel)
+				    .then(channel => {
+					    channel.send({
+						    content: `☢️ Unusually high CPM detected in space ☢️ \r\nLast reading: ${this.cpm[id].cpm}\r\n` +
+							    `🐾☢️ Alerting Furry Radiological Response Unit 🐾☢️: ${mentions}`,
+						    flags: [4096] // silenced
+					    });
+				    });
+		    });
+    };
 
-		for (const [k, v] of Object.entries(this.cpm)) {
-			radiationEmbed.addFields(
-				{ name: k, value: v, inline: true }
-			);
-		};
+    this.onDiscordMessage = (message) => {
+	    if (!message.content.startsWith("!radiation")) return;
 
-		message.reply({ embeds: [radiationEmbed] });
-		message.react('☢️');
-	};
+	    var radiationEmbed = new EmbedBuilder()
+		    .setTitle("Radiation")
+		    .setDescription("Here are the last radiation CPM readings in the space.");
+
+	    for (const [k, v] of Object.entries(this.cpm)) {
+		    radiationEmbed.addFields(
+			    { name: k, value: v.cpm, inline: true }
+		    );
+	    };
+
+	    message.reply({ embeds: [radiationEmbed] });
+	    message.react('☢️');
+    };
 };
