3bf1c974c674065db8960ffe8b56efcdceffed5b
warmans <stefan.warman@gmail.com>
2025-10-30 18:07:02 +0000

    Fix various error cases with slash commands

--- slash_commands/wiki.js
+++ slash_commands/wiki.js
@@ -1,4 +1,12 @@
-const { MessageFlags, SlashCommandBuilder, EmbedBuilder, AttachmentBuilder, ButtonBuilder, ButtonStyle, ActionRowBuilder} = require('discord.js');
+const {
+    MessageFlags,
+    SlashCommandBuilder,
+    EmbedBuilder,
+    AttachmentBuilder,
+    ButtonBuilder,
+    ButtonStyle,
+    ActionRowBuilder
+} = require('discord.js');
 const {generateQRCode} = require("../lib/qrcode");
 const {extractToolData} = require("../lib/wiki");
 
@@ -14,7 +22,7 @@ module.exports = function () {
     const baseUrl = 'https://wiki.nottinghack.org.uk/'
 
     this.configure = () => {
-        return  (new SlashCommandBuilder()
+        return (new SlashCommandBuilder()
             .setName(commandName)
             .setDescription('Find a wiki page')
             .addStringOption(option =>
@@ -66,6 +74,12 @@ module.exports = function () {
      * Handle slash command.
      */
     this.execute = async (interaction) => {
+
+        // Create "thinking..." response
+        await interaction.deferReply({
+            flags: MessageFlags.Ephemeral,
+        })
+
         const pageName = interaction.options.getString('query');
 
         const params = new URLSearchParams({
@@ -81,8 +95,27 @@ module.exports = function () {
         const page = await fetch(`${baseUrl}api.php?${params}`)
             .then(response => response.json())
             .then(results => {
-                return results.query?.pages[0]?.revisions[0]?.slots?.main || {}
+                const pages = results.query?.pages
+                if ((pages || []).length === 0) {
+                    return null;
+                }
+                const revisions = pages[0]?.revisions;
+                if ((revisions || []).length === 0) {
+                    return null;
+                }
+                return revisions[0]?.slots?.main || null;
             })
+            .catch((err) => {
+                console.error('Failed to fetch wiki page', err);
+            })
+
+        if (!page) {
+            await interaction.editReply({
+                content: `Sorry, page was not available.`,
+                flags: MessageFlags.Ephemeral,
+            });
+            return;
+        }
 
         const toolData = extractToolData(page.content, ['image'])
         const fileName = `${toFileName(pageName)}-qr.png`
@@ -97,7 +130,7 @@ module.exports = function () {
         if (toolData != null) {
             wikiEmbed.setFields(toolData)
         } else {
-            wikiEmbed.setDescription(page.content.length > 512 ? page.content.slice(0, 512)+"..." : page.content)
+            wikiEmbed.setDescription(page.content.length > 512 ? page.content.slice(0, 512) + "..." : page.content)
         }
 
         const actionsRow = new ActionRowBuilder()
@@ -109,7 +142,7 @@ module.exports = function () {
                     .setEmoji('✅')
             );
 
-        await interaction.reply({
+        await interaction.editReply({
             content: `\`Posted by ${interaction.user?.globalName ?? interaction.user?.username ?? 'Unknown'}\``,
             flags: MessageFlags.Ephemeral,
             embeds: [wikiEmbed],
